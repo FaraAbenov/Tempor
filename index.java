5c222215b856472705b8656ff44a81cb3e0397c6
FaraAbenov <FaraAbenov@users.noreply.github.com>
2021-11-17 17:50:50 +0600

    create index.java

---
+++ index.java
@@ -0,0 +1,21 @@
+    public static class Password{
+
+        private String oldValue;
+        private String newValue;
+
+        public String getNewValue() {
+            return newValue;
+        }
+
+        public void setNewValue(String newValue) {
+            this.newValue = newValue;
+        }
+
+        public String getOldValue() {
+            return oldValue;
+        }
+
+        public void setOldValue(String oldValue) {
+            this.oldValue = oldValue;
+        }
+    }
